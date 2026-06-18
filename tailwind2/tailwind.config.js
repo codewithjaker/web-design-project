8df0d0f70952f12e3c8080173774fe7043c3f9af
codewithjaker <mohamadjakerhossain86@gmail.com>
2026-06-18 17:11:24 +0600

    add more project for tailwind mastery

---
+++ tailwind2/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
